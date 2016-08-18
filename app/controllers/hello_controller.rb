992861ae873ec6ee6168b26493318d60dccd94e6
Satoshi Tanoue <again2222@gmail.com>
2016-08-19 00:26:53 +0900

    idがPOSTリクエストされてないとき，エラーが出ないように対処

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,15 +1,21 @@
 class HelloController < ApplicationController
-  def index
-	  @title ="index"
-	  @header_content ="content"
-	  @msg =  "TITLE"
-	  @hello = Helo.all
-  end
-  def show 
-	  @title ="サンプルレイアウト"
-	  @header_content="データの表示"
-	  @msg ="指定のIDのデータを表示します．"
-	  @hello = Helo.find(params['id'])
-	  # @hello = Helo.find(1)
-  end
+	def index
+		@title ="index"
+		@header_content ="content"
+		@msg =  "TITLE"
+		@hello = Helo.all
+	end
+	def show 
+		@title ="サンプルレイアウト"
+		@header_content="データの表示"
+		@msg = "指定のIDのデータを表示します．"
+		id= params['id']
+		if id.nil? then
+			@hello = Helo.find(1)
+		else  
+			@hello = Helo.find(params['id'])
+			# @hello = Helo.find(1)
+		end
+
+	end
 end
